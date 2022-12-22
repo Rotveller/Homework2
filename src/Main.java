5e90f1a67f453f89465887010791701edeea7f2e
cahas <bulatovalexsandr1999@gmail.com>
2022-12-22 22:06:24 +0400

    New commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 public class Main {
     public static void main (String[] args){
-        task1();
-        task2();
-    }
-    public static void task1(){
-        System.out.println("Задача 1");
-    }
-    public static void task2() {
-        System.out.println("Задача 2");
+        var dog = 8;
+            System.out.println(dog);
+        var cat = 3.6;
+            System.out.println(cat);
+        var paper = 763789;
+            System.out.println(paper);
+        var dog2 = 8;
+            System.out.println(dog2+4);
+        var cat2 = 3.6;
+            System.out.println(cat2+4);
+        var paper2 = 763789;
+            System.out.println(paper2+4);
+        var dog3 = 8;
+            System.out.println(dog3-3.5);
+        var cat3 = 3.6;
+            System.out.println(cat3-1.6);
+        var paper3 = 763789;
+            System.out.println(paper3-7639);
     }
 }
---
+++ src/Main2.java
@@ -0,0 +1,16 @@
+public class Main2 {
+    public static void Main2 (String[] args) {
+            var dog = 8;
+            System.out.println(dog);
+            var cat = 3.6;
+            System.out.println(cat);
+            var paper = 763789;
+            System.out.println(paper);
+            System.out.println(dog + 4);
+            System.out.println(cat + 4);
+            System.out.println(paper + 4);
+            System.out.println(dog - 3.5);
+            System.out.println(cat - 1.6);
+            System.out.println(paper - 7639);
+        }
+    }

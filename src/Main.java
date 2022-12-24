e51b5a4f8f3c1b5655594d4d7bca86b95c496296
cahas <bulatovalexsandr1999@gmail.com>
2022-12-24 22:54:25 +0400

    New commit 5

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,31 @@
 public class Main {
-    public static void main (String[] args){
-        var dog = 8;
+    public static void main (String[] args) {
+        System.out.println("Task №1");
+        double dog= 8;
         var cat = 3.6;
         var paper = 763789;
+        var friend = 19;
+        var frog = 3.5;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
-        System.out.println(dog + 4);
-        System.out.println(cat + 4);
-        System.out.println(paper + 4);
-        System.out.println(dog - 3.5);
-        System.out.println(cat - 1.6);
-        System.out.println(paper - 7639);
+        System.out.println("Task №2");
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        System.out.println("Task №3");
+        dog = dog - 3,5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+        System.out.println("Task №4");
+        System.out.println((friend + 2)/7);
+        System.out.println("Task №5");
+        System.out.println(frog * 10/7+4);
     }
 }
